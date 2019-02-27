eba65ea4e82037f06f8945bba241c00904e2ce09
Rada-Zayats <rada.zayats@i.ua>
2019-02-28 00:08:01 +0200

    add main page and mobile

--- app/js/menu-opener.js
+++ app/js/menu-opener.js
@@ -47,3 +47,29 @@ function toggleClass() {
     btnOpen.classList.toggle('none');
 }
 
+const btn_prev = document.querySelector('#gallery .buttons .prev');
+const btn_next = document.querySelector('#gallery .buttons .next ');
+
+const images = document.querySelectorAll('#gallery .photos img');
+let i = 0;
+
+btn_prev.onclick = function(){
+    images[i].style.display = 'none';
+    i--;
+
+    if(i < 0){
+        i = images.length - 1;
+    }
+    images[i].style.display = 'block';
+};
+
+btn_next.onclick = function(){
+    images[i].style.display = 'none';
+    i++;
+
+    if(i >= images.length){
+        i = 0;
+    }
+    images[i].style.display = 'block';
+};
+
